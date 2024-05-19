87e5e866215100b052753b114feaf99862eccf42
alex mason <alexmas9n@gmail.com>
2024-05-19 14:56:30 +0300

    initial market fields

---
+++ TornAPI/MarketData/Item.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace TornAPI.MarketData;
+
+public class Item {
+    [JsonProperty("cost")]
+    public int Cost { get; set; }
+
+    [JsonProperty("quantity")]
+    public int Quantity { get; set; }
+}
---
+++ TornAPI/MarketData/ItemBazaar.cs
@@ -0,0 +1,8 @@
+﻿using Newtonsoft.Json;
+
+namespace TornAPI.MarketData;
+
+public class Bazaar {
+	[JsonProperty("bazaar")]
+	public Dictionary<long, Item> BazaarItems { get; set; }
+}
