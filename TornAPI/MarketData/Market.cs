ccfcee3cbbf198c0c718b76e406e3aed91726f35
alex mason <alexmas9n@gmail.com>
2024-05-19 15:17:42 +0300

    unnecassry usings

---
+++ TornAPI/MarketData/Market.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace TornAPI.MarketData;
+
+public class Market {
+	[JsonProperty("itemmarket")]
+	public ItemMarket ItemMarket { get; set; }
+
+	[JsonProperty("bazaar")]
+	public Bazaar ItemBazaar { get; set; }
+}
